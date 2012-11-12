bd6903d832d546a50bd2b44ef23f26b913d9573d
Andrew Gallant (Ocelot) <Andrew.Gallant@tufts.edu>
2012-11-11 23:49:55 -0500

    Not sure how I missed this. Updates for the new 'seq' package.

--- fasta/fasta.go
+++ fasta/fasta.go
@@ -7,49 +7,33 @@ import (
 	"io"
 	"strings"
 	"unicode"
+
+	"github.com/BurntSushi/bcbgo/seq"
 )
 
-// An Entry corresponds to an entry in a FASTA file. That is, it is a tuple
-// of a single line header and a sequence over multiple lines concatenated
-// into a single slice of bytes.
-//
-// N.B. The header part of a FASTA entry can also be in a standard format.
-// As of right now, there are no facilities in this package to parse those
-// formats, but they are forthcoming.
-type Entry struct {
-	Header   string
-	Sequence []byte
-}
-
-// Output is a string in FASTA format, with the sequence wrapped at 60
-// columns.
-func (e Entry) String() string {
-	return e.StringCols(60)
-}
-
-// StringCols returns the FASTA string corresponding to this entry with the
+// SequenceFasta returns the FASTA string corresponding to a sequence with the
 // sequence wrapped at the number of columns given.
 //
 // If cols is <= 0, then no wrapping is done.
-func (e Entry) StringCols(cols int) string {
+func SequenceFasta(s seq.Sequence, cols int) string {
 	if cols <= 0 {
-		return fmt.Sprintf(">%s\n%s", e.Header, string(e.Sequence))
+		return fmt.Sprintf(">%s\n%s", s.Name, s.Residues)
 	}
 
-	wrapped := make([]string, 1+((len(e.Sequence)-1)/cols))
+	wrapped := make([]string, 1+((len(s.Residues)-1)/cols))
 	for i := range wrapped {
 		start := cols * i
 		end := start + cols
-		if end > len(e.Sequence) {
-			end = len(e.Sequence)
+		if end > len(s.Residues) {
+			end = len(s.Residues)
 		}
-		wrapped[i] = string(e.Sequence[start:end])
+		wrapped[i] = fmt.Sprintf("%s", s.Residues[start:end])
 	}
-	return fmt.Sprintf(">%s\n%s", e.Header, strings.Join(wrapped, "\n"))
+	return fmt.Sprintf(">%s\n%s", s.Name, strings.Join(wrapped, "\n"))
 }
 
-func (e Entry) isNull() bool {
-	return len(e.Header) == 0 && e.Sequence == nil
+func seqIsNull(s seq.Sequence) bool {
+	return len(s.Name) == 0 && s.Residues == nil
 }
 
 // A Reader reads entries from FASTA encoded input.
@@ -67,6 +51,8 @@ type Reader struct {
 	nextHeader     []byte
 }
 
+// NewReader creates a new Reader that is ready to read sequences from some
+// io.Reader.
 func NewReader(r io.Reader) *Reader {
 	return &Reader{
 		TrustSequences: false,
@@ -79,19 +65,19 @@ func NewReader(r io.Reader) *Reader {
 // ReadAll will read all entries in the FASTA input and return them as a slice.
 // If an error is encountered, processing is stopped, and the error is
 // returned.
-func (r *Reader) ReadAll() ([]Entry, error) {
-	entries := make([]Entry, 0, 100)
+func (r *Reader) ReadAll() ([]seq.Sequence, error) {
+	seqs := make([]seq.Sequence, 0, 100)
 	for {
-		entry, err := r.Read()
+		s, err := r.Read()
 		if err == io.EOF {
 			break
 		}
 		if err != nil {
 			return nil, err
 		}
-		entries = append(entries, entry)
+		seqs = append(seqs, s)
 	}
-	return entries, nil
+	return seqs, nil
 }
 
 // Read will read the next entry in the FASTA input.
@@ -116,16 +102,16 @@ func (r *Reader) ReadAll() ([]Entry, error) {
 // result in erroneous line numbers in error messages. Finally, you MUST seek
 // to a location that corresponds precisely to an entry boundary. i.e., the
 // file pointer should be at a '>' character.
-func (r *Reader) Read() (entry Entry, err error) {
-	entry, err = r.ReadEntry(TranslateNormal)
-	if !entry.isNull() {
-		return entry, nil
+func (r *Reader) Read() (s seq.Sequence, err error) {
+	s, err = r.ReadSequence(TranslateNormal)
+	if !seqIsNull(s) {
+		return s, nil
 	}
 	if err == io.EOF {
-		return Entry{}, err
+		return seq.Sequence{}, err
 	}
 	if err != nil {
-		return Entry{}, fmt.Errorf("Error on line %d: %s", r.line, err)
+		return seq.Sequence{}, fmt.Errorf("Error on line %d: %s", r.line, err)
 	}
 	panic("unreachable")
 }
@@ -138,20 +124,21 @@ func (r *Reader) SeekerReset() {
 	r.nextHeader = nil
 }
 
-// ReadEntry is exported for use in other packages that read FASTA-like files.
+// ReadSequence is exported for use in other packages that read FASTA-like
+// files.
 //
 // The 'translate' function is used when sequences are checked for valid
 // characters.
 //
 // If you're just reading FASTA files, this method SHOULD NOT be used.
-func (r *Reader) ReadEntry(translate Translator) (Entry, error) {
-	entry := Entry{}
+func (r *Reader) ReadSequence(translate Translator) (seq.Sequence, error) {
+	s := seq.Sequence{}
 	seenHeader := false
 
 	// Before entering the main loop, we have to check to see if we've
 	// already read this entry's header.
 	if r.nextHeader != nil {
-		entry.Header = trimHeader(r.nextHeader)
+		s.Name = trimHeader(r.nextHeader)
 		r.nextHeader = nil
 		seenHeader = true
 	}
@@ -159,10 +146,10 @@ func (r *Reader) ReadEntry(translate Translator) (Entry, error) {
 		line, err := r.buf.ReadBytes('\n')
 		if err == io.EOF {
 			if len(line) == 0 {
-				return entry, io.EOF
+				return s, io.EOF
 			}
 		} else if err != nil {
-			return Entry{}, err
+			return seq.Sequence{}, err
 		}
 		line = bytes.TrimSpace(line)
 
@@ -175,11 +162,12 @@ func (r *Reader) ReadEntry(translate Translator) (Entry, error) {
 		// If we haven't seen the header yet, this better be it.
 		if !seenHeader {
 			if line[0] != '>' {
-				return Entry{}, fmt.Errorf("Expected '>', got '%c'.", line[0])
+				return seq.Sequence{},
+					fmt.Errorf("Expected '>', got '%c'.", line[0])
 			}
 
 			// Trim the '>' and load this line into the header.
-			entry.Header = trimHeader(line)
+			s.Name = trimHeader(line)
 			seenHeader = true
 
 			r.line++
@@ -190,57 +178,63 @@ func (r *Reader) ReadEntry(translate Translator) (Entry, error) {
 			r.nextHeader = line
 
 			r.line++
-			return entry, nil
+			return s, nil
 		}
 
 		// Finally, time to start reading the sequence.
 		// If we trust the sequences, then we can just append this line
 		// willy nilly. Otherwise we've got to check each character.
-		if entry.Sequence == nil {
-			entry.Sequence = make([]byte, 0, 50)
+		if s.Residues == nil {
+			s.Residues = make([]seq.Residue, 0, 50)
 		}
-		if !r.TrustSequences {
-			for i, b := range line {
+		if r.TrustSequences {
+			for _, b := range line {
+				s.Residues = append(s.Residues, seq.Residue(b))
+			}
+		} else {
+			for _, b := range line {
 				bNew, ok := translate(b)
 				if !ok {
-					return Entry{},
+					return seq.Sequence{},
 						fmt.Errorf("Invalid character '%c' on line %d.",
 							b, r.line)
 				}
-				line[i] = bNew
+
+				// If the zero byte is returned from translate, then we
+				// don't keep this residue around.
+				if b > 0 {
+					s.Residues = append(s.Residues, bNew)
+				}
 			}
 		}
-
-		// Careful. Since we're using 'line...' notation, the bytes here
-		// are *copied*. Which is what we want, lest we pin our sequences
-		// so that the garbage collector can't free them.
-		entry.Sequence = append(entry.Sequence, line...)
-
 		r.line++
 	}
 	panic("unreachable")
 }
 
 // A Translator is a function that accepts a single character, checks whether
-// it's valid, and optionally maps it to a new character.
+// it's valid, and optionally maps it to a new character. Additionally, if the
+// zero byte is returned, then the character should not be included in the
+// final sequence.
 //
 // Translators are ONLY applicable to developers writing their own parsers for
 // FASTA-like files. They should not be used to read regular FASTA files.
-type Translator func(b byte) (byte, bool)
+type Translator func(b byte) (seq.Residue, bool)
 
-// TranslateNormal is the default translator for regular (and aligned) FASTA
+// TranslateNormal is the default translator for regular (NOT aligned) FASTA
 // files.
-func TranslateNormal(b byte) (byte, bool) {
+func TranslateNormal(b byte) (seq.Residue, bool) {
 	switch {
 	case b >= 'a' && b <= 'z':
-		b = byte(unicode.ToTitle(rune(b)))
+		return seq.Residue(unicode.ToTitle(rune(b))), true
 	case b >= 'A' && b <= 'Z':
+		return seq.Residue(b), true
 	case b == '*':
+		return 0, true
 	case b == '-':
-	default:
-		return 0, false
+		return '-', true
 	}
-	return b, true
+	return 0, false
 }
 
 func trimHeader(line []byte) string {
@@ -257,15 +251,21 @@ type Writer struct {
 	// The number of columns to wrap a sequence at. By default, this
 	// is set to 60. A value <= 0 will result in no wrapping.
 	Columns int
-	buf     *bufio.Writer
+
+	// Whether to a '*' at the end of each sequence.
+	// By default, this is false.
+	Asterisk bool
+
+	buf *bufio.Writer
 }
 
 // NewWriter createa a new FASTA writer that can write FASTA entries to
 // an io.Writer.
 func NewWriter(w io.Writer) *Writer {
 	return &Writer{
-		Columns: 60,
-		buf:     bufio.NewWriter(w),
+		Columns:  60,
+		Asterisk: false,
+		buf:      bufio.NewWriter(w),
 	}
 }
 
@@ -279,17 +279,22 @@ func (w *Writer) Flush() error {
 // You may need to call Flush in order for the changes to be written.
 //
 // XXX: Currently, the sequence is not checked. Should it be?
-func (w *Writer) Write(entry Entry) error {
-	s := fmt.Sprintf("%s\n", entry.StringCols(w.Columns))
-	_, err := w.buf.WriteString(s)
+func (w *Writer) Write(s seq.Sequence) error {
+	var out string
+	if w.Asterisk {
+		out = fmt.Sprintf("%s*\n", SequenceFasta(s, w.Columns))
+	} else {
+		out = fmt.Sprintf("%s\n", SequenceFasta(s, w.Columns))
+	}
+	_, err := w.buf.WriteString(out)
 	return err
 }
 
 // WriteAll writes a slice of FASTA entries to the underyling io.Writer, and
 // calls Flush.
-func (w *Writer) WriteAll(entries []Entry) error {
-	for _, entry := range entries {
-		if err := w.Write(entry); err != nil {
+func (w *Writer) WriteAll(seqs []seq.Sequence) error {
+	for _, s := range seqs {
+		if err := w.Write(s); err != nil {
 			return err
 		}
 	}
